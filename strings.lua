c0037add0a020767fc3bf8dd48696a440e2e5cf0
Florian Thiel <flth@flth.de>
2023-05-14 22:42:39 +0200

    initial

---
+++ strings.lua
@@ -0,0 +1,27 @@
+function data()
+return {
+	en = {
+		["mod_name"] = "MaK GDT",
+		["mod_desc"] = "The MaK GDT (large-capacity diesel railcar) is an internal combustion railcar for non-federal railways that was first delivered in 1953. The railcars were used on North German private railways. In terms of design, the GDT were based on the design of the central entry coaches of the Deutsche Bundesbahn. The diesel engines each drove the axles on the inside of the bogies, the maximum speed was around 75 km/h.",
+
+        ["menu_MaK_GDT"] = "MaK GDT",
+		["GDT1_name"] = "MaK GDT",
+		["GDT1_desc"] = "The MaK large-capacity diesel railcars were internal combustion railcars for non-federal railways that were delivered from 1953. You were mainly traveling in northern Germany.",
+
+		["Kaffkieker_name"] = "MaK GDT \"Kaffkieker\"",
+		["Kaffkieker_desc"] = "At German Museum Railway (DEV) in Bruchhausen-Vilsen, a MaK GDT drives as \"Kaffkieker\" on the normal-gauge route in excursion traffic.",
+
+	},
+	de = {
+		["mod_name"] = "MaK GDT",
+		["mod_desc"] = "Der MaK GDT (Großraum-Dieseltriebwagen) ist ein im Jahr 1953 erstmals ausgelieferter Verbrennungstriebwagen für Nichtbundeseigene Eisenbahnen. Eingesetzt wurden die Triebwagen bei norddeutschen Privatbahnen. Konstruktiv waren die GDT angelehnt an die Konstruktion der Mitteleinstiegswagen der Deutschen Bundesbahn. Die Dieselmotoren trieben jeweils die innenliegenden Achsen der Drehgestelle an, die Höchstgeschwindigkeit lag bei ca. 75 km/h.",
+
+        ["menu_MaK_GDT"] = "MaK GDT",
+		["GDT1_name"] = "MaK GDT",
+		["GDT1_desc"] = "Die MaK Großraum-Dieseltriebwagen waren ab 1953 ausgelieferte Verbrennungstriebwagen für Nichtbundeseigene Eisenbahnen. Sie waren vor allem im norddeutschen Raum unterwegs.",
+
+		["Kaffkieker_name"] = "MaK GDT \"Kaffkieker\"",
+		["Kaffkieker_desc"] = "Bei der DEV in Bruchhausen-Vilsen fährt ein MaK GDT als \"Kaffkieker\" auf der normalspurigen Strecke im Ausflugsverkehr.",
+	},
+}
+end
